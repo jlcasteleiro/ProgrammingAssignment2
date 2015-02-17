b154d0e67b2a94556286aa824e755de60ff85555
Josè Luis Casteleiro <jlcasteleiro@gmail.com>
2015-02-17 16:28:12 +0100

    Assignment2_finished

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function will return the inverse of a matrix, but before computing
+## the inverse, if check in the cache if this inverse matrix was calculated
+## before to avoid re-calculating
 
-## Write a short comment describing this function
+## The base function is the same that in the instructions of the assigment,
+## I will modify step by step the function to addapt to my desires
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { ## The input is a matrix
+    m <- NULL
+    set <- function(y) { ## In the first time that I call the function, it
+        x <<- y          ## saves the variable
+        m <<- NULL
+    }
+    get <- function() x ## This is the function to get the value of the variable
+    setsolve <- function(solve) m <<- solve ## This is to set the inverse
+    getsolve <- function() m ## This is to get the inverse calculated before
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve) ## The function returns a list with the four functions mentioned
 }
 
 
-## Write a short comment describing this function
+## The base function is the same that in the instructions of the assigment
+## I will modify step by step the function to addapt to my desires
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { ## The input is the variable asigned with the last function
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve() ## The funcion recover the value from cache
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m) ## If the inverse was calculated, it returns it with a message
+    }
+    data <- x$get() ## If the inverse is not calculated yet, it recover the data
+    m <- solve(data, ...) ## and calculated the corresponding inverse
+    x$setsolve(m) ## Then, the value is stored to avoid recurrent calculation
+    m
 }
+
+## The ussage of this function could be explained with one example.
+## If the variable you want to calculate the inverse is:
+
+##   [1 2 3 4 5]
+##   [2 3 4 5 1]
+## A=[3 4 5 1 2]
+##   [4 5 1 2 3]
+##   [5 1 2 3 4]
+
+## The code should be:
+## 'A <- matrix(c(1,2,3,4,5,2,3,4,5,1,3,4,5,1,2,4,5,1,2,3,5,1,2,3,4),5,5)'
+## 'a <- makeCacheMatrix(A)'
+## 'cacheSolve(a)'
+
+## The first time, the inverse was calculated, but the next times, the result was cached
